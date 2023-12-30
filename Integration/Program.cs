6eddf8483a5c8e4b22f40d9fbab555f70de354ac
Serux <serchi93@hotmail.com>
2023-12-30 23:08:52 +0100

    added code to  Program.cs to demonstrate the parallel insertion as asked in section 1.c of the Readme

--- Integration/Program.cs
+++ Integration/Program.cs
@@ -24,6 +24,21 @@ public abstract class Program
 
         service.GetAllItems().ForEach(Console.WriteLine);
 
+        //To check the parallel insertion of elements, we can queue
+        // some more elements and wait for 2.1 seconds, as each
+        // element needs 2 seconds, everything should be added after
+        // that time if it's being inserted in parallel.
+
+        
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("x"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("y"));
+        ThreadPool.QueueUserWorkItem(_ => service.SaveItem("z"));
+        Console.WriteLine("Everything before 2.1 seconds:");
+        service.GetAllItems().ForEach(Console.WriteLine);
+        Thread.Sleep(2_100);
+        Console.WriteLine("Everything after 2.1 seconds:");
+        service.GetAllItems().ForEach(Console.WriteLine);
+
         Console.ReadLine();
     }
 }
